23aab06189154806d86f66ce884ea1c0d058f4c5
Wisdom Mbuk <wisdommbuk14@gmail.com>
2023-06-29 07:05:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - concatenates two strings
@@ -6,24 +7,32 @@
  * @src: input value
  * Return: void
  */
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+char* _strcat(char* dest, const char* src) {
+    char* ptr = dest;
 
-	dest[i] = '\0';
-	return (dest);
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+    *ptr = '\0';
+
+    return dest;
+}
+
+int main() {
+    char dest[20] = "Hello, ";
+    const char src[] = "world!";
+
+    printf("Before concatenation: %s\n", dest);
+    _strcat(dest, src);
+    printf("After concatenation: %s\n", dest);
+
+    return 0;
 }
